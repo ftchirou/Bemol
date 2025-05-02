84d5ad1686f86ea71299d6550d29ea5fdc025381
Faiçal Tchirou <ftchirou@gmail.com>
2025-05-02 22:26:07 +0200

    Enable the keyboard on the accuracy screen

--- Bemol/Screens/AccuracyScreen.swift
+++ Bemol/Screens/AccuracyScreen.swift
@@ -50,11 +50,12 @@ final class AccuracyScreen {
   }()
 
   private lazy var keyboardView: KeyboardView = {
-    let keyboardView = KeyboardView(range: 1...2)
+    let keyboardView = KeyboardView(range: range)
     keyboardView.translatesAutoresizingMaskIntoConstraints = false
     keyboardView.setEnabledForAllKeys(false)
     keyboardView.setTintForAllNotes(nil)
     keyboardView.isScrollEnabled = false
+    keyboardView.delegate = self
 
     return keyboardView
   }()
@@ -110,6 +111,7 @@ final class AccuracyScreen {
 
   // MARK: - Properties
 
+  private let range: ClosedRange<Octave> = 1...2
   private let notePlayer: NotePlayer
 
   private var accuracy: [Note: Double] = [:] {
